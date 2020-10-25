4801c312bf33043f496b7dbe359c62548a846b41
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2020-10-25 06:39:27 +0530

    Date formattng is done

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 export const COLUMNS = [{
         Header: 'Id',
         Footer: 'Id',
@@ -16,7 +18,8 @@ export const COLUMNS = [{
     {
         Header: 'Date Of Birth',
         Footer: 'Date Of Birth',
-        accessor: 'date_of_birth'
+        accessor: 'date_of_birth',
+        Cell: ({ value }) => { return format(new Date(value), 'dd/MM/yyyy') }
 
     },
     {
